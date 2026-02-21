9e2e77646e2aa871dba9e35f5e2991f25494f799
Eugeniusz Dzikowicz <eugeniusz.dzikowicz@gmail.com>
2026-02-21 12:47:43 +0100

    Task solution improved

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -6,6 +6,6 @@ public class ColorSupplier {
     private static final Random random = new Random();
 
     public String getRandomColor() {
-        return Colors.values()[random.nextInt(Colors.values().length)].toString().toLowerCase();
+        return Colors.values()[random.nextInt(Colors.values().length)].name().toLowerCase();
     }
 }
--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -4,47 +4,52 @@ import java.util.Random;
 
 public class FigureSupplier {
     private static final Random random = new Random();
+    private static final int FIGURE_TYPES_COUNT = 5;
+    private static final int MIN_SIZE = 1;
+    private static final int MAX_SIZE = 10;
+    private static final String DEFAULT_COLOR = "white";
+    private static final int DEFAULT_RADIUS = 10;
     private final ColorSupplier colorSupplier = new ColorSupplier();
 
     public Figure getRandomFigure() {
 
-        int figureType = random.nextInt(5);
+        int figureType = random.nextInt(FIGURE_TYPES_COUNT);
 
         switch (figureType) {
             case 0:
                 return new Rectangle(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(10),
-                        random.nextInt(10));
+                        random.nextInt(MAX_SIZE) + MIN_SIZE,
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
 
             case 1:
                 return new Square(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(10));
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
 
             case 2:
                 return new Circle(
                         colorSupplier.getRandomColor(),
-                        random.nextInt(10));
+                        random.nextInt(MAX_SIZE) + MIN_SIZE);
 
             case 3:
                 return new IsoscelesTrapezoid(
                        colorSupplier.getRandomColor(),
-                       random.nextInt(10), random.nextInt(10),
-                       random.nextInt(10));
+                       random.nextInt(MAX_SIZE) + MIN_SIZE, random.nextInt(MAX_SIZE) + MIN_SIZE,
+                       random.nextInt(MAX_SIZE) + MIN_SIZE);
 
             case 4:
                 return new RightTriangle(
                        colorSupplier.getRandomColor(),
-                       random.nextInt(10),
-                       random.nextInt(10));
+                       random.nextInt(MAX_SIZE) + MIN_SIZE,
+                       random.nextInt(MAX_SIZE) + MIN_SIZE);
 
             default:
-                return null;
+                return getDefaultFigure();
         }
     }
 
     public Figure getDefaultFigure() {
-        return new Circle("white", 10);
+        return new Circle(DEFAULT_COLOR, DEFAULT_RADIUS);
     }
 }
